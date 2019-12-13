862d8ab228ceb0f7bb46cfe8f33b01d598307dfd
Ogier Bouvier <ars3nic@ars3nic.ch>
2019-12-13 17:00:21 +0100

    cleanup lib.rs
    Signed-off-by: Ogier Bouvier <ars3nic@ars3nic.ch>

--- src/lib.rs
+++ src/lib.rs
@@ -16,53 +16,27 @@
 //! [dependencies]
 //! utp = "*"
 //! ```
-//!
-//! # Examples
-//!
-//! ```no_run
-//! extern crate utp;
-//!
-//! use utp::UtpStream;
-//! use std::io::Write;
-//!
-//! fn main() {
-//!     // Connect to an hypothetical local server running on port 8080
-//!     let addr = "127.0.0.1:8080";
-//!     let mut stream = UtpStream::connect(addr).expect("Error connecting to remote peer");
-//!
-//!     // Send a string
-//!     stream.write("Hi there!".as_bytes()).expect("Write failed");
-//!
-//!     // Close the stream
-//!     stream.close().expect("Error closing connection");
-//! }
-//! ```
-//!
-//! Note that you can easily convert a socket to a stream using the `Into` trait, like so:
-//!
-//! ```no_run
-//! # use utp::{UtpStream, UtpSocket};
-//! let socket = UtpSocket::bind("0.0.0.0:0").expect("Error binding socket");
-//! let stream: UtpStream = socket.into();
-//! ```
-
 #![deny(missing_docs)]
-
 // Optional features
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
-#![cfg_attr(feature = "clippy", allow(len_without_is_empty, doc_markdown, needless_return,
-                                      transmute_ptr_to_ref))]
 #![cfg_attr(feature = "unstable", feature(test))]
+#![cfg_attr(
+    feature = "clippy",
+    allow(
+        len_without_is_empty,
+        doc_markdown,
+        needless_return,
+        cast_ptr_alignment,
+    )
+)]
 
-extern crate rand;
-extern crate num_traits;
-#[macro_use] extern crate log;
-#[cfg(test)] extern crate quickcheck;
+#[macro_use]
+extern crate log;
 
 // Public API
-pub use socket::UtpSocket;
 pub use socket::UtpListener;
+pub use socket::UtpSocket;
 
 mod bit_iterator;
 mod error;
